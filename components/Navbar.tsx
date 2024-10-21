3185ed364dda6ab752643b6d57f660af19b58abd
unknown <ariyeric@gmail.com>
2024-10-21 12:47:23 +0100

    first commit

---
+++ components/Navbar.tsx
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+     <nav className='border-2 border-red-500 flexBetween max-container padding-container'>
+        <Link href='/'>
+        
+        </Link>
+     </nav>
+    </>
+  )
+}
+
+export default Navbar
